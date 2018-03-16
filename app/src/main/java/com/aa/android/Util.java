a806ff4b502b0fe27c24e4adb6b9ee94ed976b66
Trevor Xiao <txiao@redhat.com>
2018-03-16 17:08:21 -0400

    Update delay time

--- app/src/main/java/com/aa/android/Util.java
+++ app/src/main/java/com/aa/android/Util.java
@@ -53,8 +53,8 @@ public class Util {
 
         ComponentName serviceComponent = new ComponentName(context, LockedNotificationTimerService.class);
         JobInfo.Builder builder = new JobInfo.Builder(0, serviceComponent);
-        builder.setMinimumLatency(1 * 3600 * 1000); // wait at least
-        builder.setOverrideDeadline(2 * 3600 * 1000); // maximum delay
+        builder.setMinimumLatency(3600 * 1000); // wait at least
+        builder.setOverrideDeadline(1800 * 1000); // maximum delay
         //builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED); // require unmetered network
         //builder.setRequiresDeviceIdle(true); // device should be idle
         //builder.setRequiresCharging(false); // we don't care if the device is charging or not
